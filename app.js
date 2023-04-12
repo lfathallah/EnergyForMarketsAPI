3e89a0aa1c7716b43c1e568e617dd823f620d162
lfathallah <linda.fathallah@hotmail.fr>
2023-04-13 00:04:25 +0200

    Create new park

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from "express";
-import cors from "cors";
 import {connectAndInit} from "./utils/db-connect.js";
+import {createPark} from "./persistence/ParkRepository.js";
 import bodyParser from "body-parser";
 
 const port = 3000;
@@ -18,8 +18,23 @@ app.use(cors());
  })
 
 app.get('/', (req, res) => res.send("Welcome to the energy offers manager"));
+app.post('/parks/new', (req, res) => addPark(req, res));
 
- app.listen(port, function(err){
+async function addPark(req, res) {
+    let name = req.body.name;
+
+    try {
+        console.log(`Creating new Energy Park with name ${name}`);
+        let park = await createPark(req.body);
+        console.log(`Park created successfully`);
+        res.send(`The new Energy Park with name ${park.name} has been created successfully with an auto-generated ID [${park.id}]`);
+    } catch (error) {
+        console.log(`Error creating new Energy Park with name ${name}`, error);
+        res.status(500).send(`Error creating new Energy Park with name ${name}`);
+    }
+}
+
+app.listen(port, function(err){
     console.log("Listening on port ", port);
     if (err) {
         console.log("Error in server setup");
--- persistence/ParkRepository.js
+++ persistence/ParkRepository.js
@@ -1,1 +1,9 @@
 import EnergyPark from "../domain/EnergyPark.js";
+
+export async function createPark(data) {
+    if (data) {
+        const park = await EnergyPark.create({name: data.name, address: "any", type: "1"})
+        console.log("Created new Energy Park with auto-generated ID:", park.id);
+        return park;
+    }
+}
